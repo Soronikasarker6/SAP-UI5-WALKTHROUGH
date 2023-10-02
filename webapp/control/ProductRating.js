4dbd55547a8205d88f3876e1fc5b594f51dae172
soronikasarkerpinkey <spinkey@schertech.com>
2023-10-02 11:11:43 +0600

    S33: Custom Controls

---
+++ webapp/control/ProductRating.js
@@ -0,0 +1,95 @@
+sap.ui.define([
+	"sap/ui/core/Control",
+	"sap/m/RatingIndicator",
+	"sap/m/Label",
+	"sap/m/Button"
+], function (Control, RatingIndicator, Label, Button) {
+	"use strict";
+	return Control.extend("sap.ui.demo.walkthrough.control.ProductRating", {
+//metadata defines the data structure and thus the API of the control
+		metadata : {
+            properties : {
+				value: 	{type : "float", defaultValue : 0}
+			},
+			aggregations : {
+				_rating : {type : "sap.m.RatingIndicator", multiple: false, visibility : "hidden"},
+				_label : {type : "sap.m.Label", multiple: false, visibility : "hidden"},
+				_button : {type : "sap.m.Button", multiple: false, visibility : "hidden"}
+			},
+			events : {
+				change : {
+					parameters : {
+						value : {type : "int"}
+					}
+				}
+			}
+		},
+
+        // whenever the control is instantiated.
+		init : function () {
+			this.setAggregation("_rating", new RatingIndicator({
+				value: this.getValue(),
+				iconSize: "2rem",
+				visualMode: "Half",
+				liveChange: this._onRate.bind(this)
+			}));
+			this.setAggregation("_label", new Label({
+				text: "{i18n>productRatingLabelInitial}"
+			}).addStyleClass("sapUiSmallMargin"));
+			this.setAggregation("_button", new Button({
+				text: "{i18n>productRatingButton}",
+				press: this._onSubmit.bind(this)
+			}).addStyleClass("sapUiTinyMarginTopBottom"));
+		},
+        setValue: function (fValue) {
+			this.setProperty("value", fValue, true);
+			this.getAggregation("_rating").setValue(fValue);
+
+			return this;
+		},
+
+		reset: function () {
+			var oResourceBundle = this.getModel("i18n").getResourceBundle();
+
+			this.setValue(0);
+			this.getAggregation("_label").setDesign("Standard");
+			this.getAggregation("_rating").setEnabled(true);
+			this.getAggregation("_label").setText(oResourceBundle.getText("productRatingLabelInitial"));
+			this.getAggregation("_button").setEnabled(true);
+		},
+
+		_onRate : function (oEvent) {
+			var oRessourceBundle = this.getModel("i18n").getResourceBundle();
+			var fValue = oEvent.getParameter("value");
+
+			this.setProperty("value", fValue, true);
+
+			this.getAggregation("_label").setText(oRessourceBundle.getText("productRatingLabelIndicator", [fValue, oEvent.getSource().getMaxValue()]));
+			this.getAggregation("_label").setDesign("Bold");
+		},
+
+		_onSubmit : function (oEvent) {
+			var oResourceBundle = this.getModel("i18n").getResourceBundle();
+
+			this.getAggregation("_rating").setEnabled(false);
+			this.getAggregation("_label").setText(oResourceBundle.getText("productRatingLabelFinal"));
+			this.getAggregation("_button").setEnabled(false);
+			this.fireEvent("change", {
+				value: this.getValue()
+			});
+		},
+
+// renderer defines the HTML structure that will be added to the DOM tree of your app
+// whenever the control is instantiated in a view. Called  whenever a property of the control is changed
+		renderer : function (oRm, oControl) {
+            //oRM can be used to write strings and control properties to the HTML page.
+            oRm.openStart("div", oControl);
+			oRm.class("myAppDemoWTProductRating");
+			oRm.openEnd();
+			oRm.renderControl(oControl.getAggregation("_rating"));
+			oRm.renderControl(oControl.getAggregation("_label"));
+			oRm.renderControl(oControl.getAggregation("_button"));
+			oRm.close("div");
+		}
+	});
+});
